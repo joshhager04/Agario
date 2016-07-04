2c00963429ef5b612b7fffde9df1e473d5d7a29e
Andrew54757 <andrews54757@gmail.com>
2016-07-03 20:17:34 -0400

    Update FakeChild.js

--- src/ai/FakeChild.js
+++ src/ai/FakeChild.js
@@ -35,52 +35,25 @@ heartbeat() {
 }
 
 getCellsInRange(cell,gameServer) {
-var id = this.getnextid();
-this.idData[id] = cell;
-  var result = {
-    cells: [],
-    processID: id,
-    action: "getcellsinrange",
-    config: gameServer.config,
-    mass: cell.mass,
-    id: cell.getId(),
-    haveTeams: gameServer.gameMode.haveTeams,
-    position: cell.position,
-    mass: cell.mass,
-    SquareSize: cell.getSquareSize(),
-    type: cell.cellType,
-    EatingRange: cell.getEatingRange(),
-    owner: {
-      id: cell.owner.pID,
-      recombineinstant: cell.owner.recombineinstant,
-      team: cell.owner.team,
-    },
-    SquareSize: cell.getSquareSize(),
-  };
-  cell.owner.visibleNodes.forEach((check)=> {
-if (!check) return;
-    var a = {
-    mass: check.mass,
-    id: check.getId(),
-    position: check.position,
-    mass: check.mass,
-    owner: [],
-    SquareSize: check.getSquareSize(),
-    type: check.cellType,
-    EatingRange: check.getEatingRange(),
-    };
-    if (check.owner) {
-      a.owner = {
-      id: check.owner.pID,
-      recombineinstant: check.owner.recombineinstant,
-      team: check.owner.team,
-      };
-    }
-    result.cells.push(a);
-  });
-  
-this.child.send(result);
-  
+  let client = cell.owner;
+      cell.calcMove(client.mouse.x, client.mouse.y, gameServer);
+
+      // Check if cells nearby
+      let list = gameServer.getCellsInRange(cell);
+      list.forEach((check)=> {
+        if (check.cellType === 0 && (client != check.owner) && (cell.mass < check.mass * gameServer.config.sizeMult) && gameServer.config.playerRecombineTime !== 0) { //extra check to make sure popsplit works by retslac
+          check.inRange = false;
+          return;
+        }
+
+        // Consume effect
+        check.onConsume(cell, gameServer);
+
+        // Remove cell
+        check.setKiller(cell);
+        gameServer.removeNode(check);
+        
+      });
   
 }
 
