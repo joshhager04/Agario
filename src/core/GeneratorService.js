b7418ee9726e211cb3474505294728f529dde7ec
Agarian <tommyb3751@gmail.com>
2016-07-21 21:55:08 -0400

    Small format mistake

--- src/core/GeneratorService.js
+++ src/core/GeneratorService.js
@@ -76,16 +76,16 @@ module.exports = class GeneratorService {
   }
 
   spawnFood() {
-  this.foodSpawned++;
-  let f = new Entity.Food(this.gameServer.getWorld().getNextNodeId(), null, utilities.getRandomPosition(this.config.borderRight, this.config.borderLeft, this.config.borderBottom, this.config.borderTop), this.config.foodMass, this.gameServer);
-  if (this.config.playerOldColors == 1) {
-    f.setColor(this.gameServer.getRandomColor());
-  } else {
-    f.setColor(utilities.getRandomColor());
+    this.foodSpawned++;
+    let f = new Entity.Food(this.gameServer.getWorld().getNextNodeId(), null, utilities.getRandomPosition(this.config.borderRight, this.config.borderLeft, this.config.borderBottom, this.config.borderTop), this.config.foodMass, this.gameServer);
+    if (this.config.playerOldColors == 1) {
+      f.setColor(this.gameServer.getRandomColor());
+    } else {
+      f.setColor(utilities.getRandomColor());
+    }
+    this.gameServer.addNode(f);
+    this.gameServer.currentFood++;
   }
-  this.gameServer.addNode(f);
-  this.gameServer.currentFood++;
-}
 
   virusCheck() {
     // Checks if there are enough viruses on the map
